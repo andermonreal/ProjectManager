3c93b18da94f3d48ed8f3cbc9f5762701ad3b92c
andermonreal <andermonreal2003@gmail.com>
2024-11-30 17:40:44 +0100

    Actualizar repositorio

--- docker.sh
+++ docker.sh
@@ -4,4 +4,4 @@ docker stop apache-container
 docker rm apache-container
 docker rmi custom-apache-container:latest
 docker build -t custom-apache-container .
-docker run -d --name apache-container -p 8080:80 -v ./public:/var/www/html custom-apache-container
+docker run -d --name apache-container -p 8080:80 -v ./webAPP:/var/www/html custom-apache-container

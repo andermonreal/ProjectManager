aa4327b90b0fe7f6cdd4947270cef70105caf0f2
andermonreal <andermonreal2003@gmail.com>
2024-12-11 17:21:42 +0100

    Cambiar instalacion

--- instalacion.sh
+++ instalacion.sh
@@ -32,17 +32,6 @@ WantedBy=multi-user.target" > /etc/systemd/system/flagsGen.service
 
 
 
-echo "#!/bin/bash
-
-echo \"ssi{$(openssl rand -hex 8)}\" > /home/www-data/user.txt
-chown www-data:www-data /home/www-data/user.txt
-chmod 600 /home/www-data/user.txt
-
-echo \"ssi{$(openssl rand -hex 8)}\" > /root/root.txt
-chmod 600 /root/root.txt
-
-gcc -fno-stack-protector -z execstack -o /home/www-data/rootAuth /home/www-data/rootAuth.c
-chmod u+s /home/www-data/rootAuth" > /usr/local/bin/flagsGen.sh
-
+cp /home/user1/ProjectManager/flagsGen.sh /usr/local/bin/
 chmod +x /usr/local/bin/flagsGen.sh
 systemctl enable flagsGen.service
